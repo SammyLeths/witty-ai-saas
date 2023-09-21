0fc5773c63c30e7b9d81c5468d88ee148987e7d7
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-09-21 21:55:26 +0100

    added landing credit

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,9 @@
 import LandingContent from "@/components/landing-content";
 import LandingHero from "@/components/landing-hero";
 import { LandingNavbar } from "@/components/landing-navbar";
+import Link from "next/link";
+
+const credit = "<SammyLeths />";
 
 const LandingPage = () => {
   return (
@@ -8,6 +11,16 @@ const LandingPage = () => {
       <LandingNavbar />
       <LandingHero />
       <LandingContent />
+      <div className="text-center flex justify-center items-center text-sm text-white py-10">
+        A project by &nbsp;
+        <Link
+          className="font-bold hover:text-blue-500"
+          href="https://www.sammyleths.com"
+          target="_blank"
+        >
+          {credit}
+        </Link>
+      </div>
     </div>
   );
 };
